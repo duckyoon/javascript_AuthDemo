4b9e6c7c2c8844c0fa241ecb7776b71cd6df0cd3
duckyoon <dkdlel616@gmail.com>
2025-03-11 14:30:22 +0900

    250311_AuthDemo_User Register

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+mongoose.connect('mongodb://localhost:27017/authDemo', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("MONGO CONNECTION OPEN!!!")
+})
+.catch(err => {
+    console.log('MONGO CONNECTION ERROR!!!')
+    console.log(err)
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('This is home page!!!!')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash,
+    })
+    await user.save()
+    res.redirect('/');
 })
 
 app.get('/secrets', (req, res) => {
