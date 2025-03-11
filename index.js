ad15a9ba75f59fe95ddacbf8aca209a316512394
duckyoon <dkdlel616@gmail.com>
2025-03-11 15:01:30 +0900

    250311_AuthDeom_Login

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost:27017/authDemo', { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://localhost:27017/authDemo')
 .then(() => {
     console.log("MONGO CONNECTION OPEN!!!")
 })
@@ -20,6 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('This is home page!!!!')
@@ -38,6 +36,22 @@ app.post('/register', async (req, res) => {
     })
     await user.save()
     res.redirect('/');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare( password, user.password )
+    if (validPassword) {
+        res.send('login')
+    }
+    else{
+        res.send('Fail')
+    }
 })
 
 app.get('/secrets', (req, res) => {
